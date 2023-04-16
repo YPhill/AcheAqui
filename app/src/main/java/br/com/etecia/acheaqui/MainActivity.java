fcdcdf1358c0f01465e716ff9af6ed342c9a10d2
yphill <phelipequeiroz2004@gmail.com>
2023-04-16 19:48:45 -0300

    navigation funcionando

--- app/src/main/java/br/com/etecia/acheaqui/MainActivity.java
+++ app/src/main/java/br/com/etecia/acheaqui/MainActivity.java
@@ -1,13 +1,19 @@
 package br.com.etecia.acheaqui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,13 +22,16 @@ public class MainActivity extends AppCompatActivity {
 
     DrawerLayout drawerLayout;
 
-
+    NavigationView NavigationView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavigationView = findViewById(R.id.navMenu);
+
 
         toolbar = findViewById(R.id.toolBar);
 
@@ -32,8 +41,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-
-
             }
         });
 
@@ -41,6 +48,38 @@ public class MainActivity extends AppCompatActivity {
                 drawerLayout, toolbar, R.string.navopen, R.string.navclose);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+
+        NavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.mLocal:
+                        startActivity(new Intent(getApplicationContext(),MainBottomActivity.class));
+                        return true;
+                    case R.id.mItensPd:
+                        startActivity(new Intent(getApplicationContext(),MainBottomActivity.class));
+                        return true;
+                    case R.id.mPerdiItm:
+                        startActivity(new Intent(getApplicationContext(),MainBottomActivity.class));
+                        return true;
+
+                    case R.id.mNotifcacoes:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        return true;
+                    case R.id.mSobre:
+                        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        return true;
+                }
+                return false;
+            }
+        });
+
+
+
+
+
+
 
 
     }
